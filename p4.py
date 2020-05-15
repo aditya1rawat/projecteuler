3426d908bfd1df85dce0044a4948152cd9718dee
Aditya Rawat <aditya1rawat@gmail.com>
2020-05-14 21:10:56 -0700

    started question 4

---
+++ p4.py
@@ -0,0 +1,13 @@
+# number = 12345
+
+# while number > 0:
+#     last_digit = number % 10
+#     reverse = (reverse * 10) + last_digit
+#     number /= 10
+
+# print(reverse)
+
+# # largest = 0
+
+for x in range(1, 101):
+    for y in range(1, 101):
